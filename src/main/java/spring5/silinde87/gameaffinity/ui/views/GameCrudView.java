51d5e7ec61d90b77bdad4c202dd828936b3d3467
Silinde87 <paurodriguezmolina@gmail.com>
2020-11-01 11:45:45 +0100

    Updated Game and Genre CrudView

--- src/main/java/spring5/silinde87/gameaffinity/ui/views/GameCrudView.java
+++ src/main/java/spring5/silinde87/gameaffinity/ui/views/GameCrudView.java
@@ -1,13 +1,17 @@
 package spring5.silinde87.gameaffinity.ui.views;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import org.vaadin.crudui.crud.CrudOperation;
 import org.vaadin.crudui.crud.impl.GridCrud;
 import org.vaadin.crudui.form.CrudFormFactory;
-import spring5.silinde87.gameaffinity.backend.domain.Game;
-import spring5.silinde87.gameaffinity.backend.services.GameServiceImpl;
+import org.vaadin.crudui.form.impl.field.provider.ComboBoxProvider;
+import spring5.silinde87.gameaffinity.backend.domain.*;
+import spring5.silinde87.gameaffinity.backend.services.*;
 import spring5.silinde87.gameaffinity.ui.MainView;
 
 @Route(value = "games", layout = MainView.class)
@@ -15,11 +19,58 @@ import spring5.silinde87.gameaffinity.ui.MainView;
 @RouteAlias(value = "", layout = MainView.class)
 public class GameCrudView extends HorizontalLayout {
 
-    public GameCrudView(GameServiceImpl gameService){
-        GridCrud<Game> crud = new GridCrud<>(Game.class,gameService);
+    public GameCrudView(GameServiceImpl gameService, DeveloperServiceImpl developerService,
+                        ProducerServiceImpl producerService, GenreServiceImpl genreService,
+                        PlatformServiceImpl platformService){
+
+        //Crud Instance
+        GridCrud<Game> crud = new GridCrud<>(Game.class);
+
+        //Grid Configuration
+        crud.getGrid().setColumns("name","developer", "producer", "genre", "platform", "releaseDate");
+        crud.getGrid().getColumns().forEach(col -> col.setAutoWidth(true));
+
+        //Form Configuration
+        crud.getCrudFormFactory().setUseBeanValidation(false);
+        crud.getCrudFormFactory().setVisibleProperties(
+                "name","developer", "producer", "genre", "platform", "releaseDate");
+
+        crud.getCrudFormFactory().setFieldProvider("developer",
+                new ComboBoxProvider<>(developerService.findAll()));
+        crud.getCrudFormFactory().setFieldProvider("developer",
+                new ComboBoxProvider<>("Developer", developerService.findAll(),
+                        new TextRenderer<>(Developer::getName), Developer::getName));
+
+        crud.getCrudFormFactory().setFieldProvider("producer",
+                new ComboBoxProvider<>(producerService.findAll()));
+        crud.getCrudFormFactory().setFieldProvider("producer",
+                new ComboBoxProvider<>("Producer", producerService.findAll(),
+                        new TextRenderer<>(Producer::getName), Producer::getName));
+
+        crud.getCrudFormFactory().setFieldProvider("genre",
+                new ComboBoxProvider<>(genreService.findAll()));
+        crud.getCrudFormFactory().setFieldProvider("genre",
+                new ComboBoxProvider<>("Genres", genreService.findAll(),
+                        new TextRenderer<>(Genre::getName), Genre::getName));
+
+        crud.getCrudFormFactory().setFieldProvider("platform",
+                new ComboBoxProvider<>(platformService.findAll()));
+        crud.getCrudFormFactory().setFieldProvider("platform",
+                new ComboBoxProvider<>("Platforms", platformService.findAll(),
+                        new TextRenderer<>(Platform::getName), Platform::getName));
+
+
+        //Layout Configuration
         add(crud);
-
-
+        //crud.setFindAllOperationVisible(false);
         setSizeFull();
+
+        //Logic Configuration
+        crud.setOperations(
+                () -> gameService.findAll(),
+                game -> gameService.add(game),
+                game -> gameService.update(game),
+                game -> gameService.delete(game)
+        );
     }
 }
--- src/main/java/spring5/silinde87/gameaffinity/ui/views/GenreCrudView.java
+++ src/main/java/spring5/silinde87/gameaffinity/ui/views/GenreCrudView.java
@@ -1,21 +1,68 @@
 package spring5.silinde87.gameaffinity.ui.views;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import org.vaadin.crudui.crud.impl.GridCrud;
+import org.vaadin.crudui.form.impl.field.provider.CheckBoxGroupProvider;
+import org.vaadin.crudui.form.impl.field.provider.ComboBoxProvider;
+import spring5.silinde87.gameaffinity.backend.domain.Game;
 import spring5.silinde87.gameaffinity.backend.domain.Genre;
+import spring5.silinde87.gameaffinity.backend.services.GameServiceImpl;
 import spring5.silinde87.gameaffinity.backend.services.GenreServiceImpl;
 import spring5.silinde87.gameaffinity.ui.MainView;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Route(value = "genres", layout = MainView.class)
 @PageTitle("Genres List")
 public class GenreCrudView extends HorizontalLayout {
 
-    public GenreCrudView(GenreServiceImpl genreService){
-        GridCrud<Genre> crud = new GridCrud<>(Genre.class,genreService);
+
+    private GenreServiceImpl genreService;
+    private GameServiceImpl gameService;
+
+
+    public GenreCrudView(GenreServiceImpl genreService, GameServiceImpl gameService){
+        this.genreService = genreService;
+        this.gameService = gameService;
+
+        GridCrud<Genre> crud = new GridCrud<>(Genre.class, genreService);
+
+        //Grid Configuration
+        crud.getGrid().setColumns("name");
+        crud.getGrid().addColumn(genre -> {
+            Set<Game> game = genre.getGameList();
+            return game == null ? "-" : game.stream().map(Game::getName).collect(Collectors.joining(", "));
+        }).setHeader("Game List");
+
+        //Form Configuration
+        crud.getCrudFormFactory().setUseBeanValidation(false);
+        crud.getCrudFormFactory().setVisibleProperties("name", "gameList");
+
+        crud.getCrudFormFactory().setFieldProvider("gameList",
+                new CheckBoxGroupProvider<>(gameService.findAll()));
+        crud.getCrudFormFactory().setFieldProvider("gameList",
+                new CheckBoxGroupProvider<>("Game List", gameService.findAll(), Game::getName));
+
+
+        //Layout Configuration
         add(crud);
+        //crud.setFindAllOperationVisible(false);
         setSizeFull();
+
+        //Logic Configuration
+        crud.setOperations(
+                () -> genreService.findAll(),
+                genre -> genreService.add(genre),
+                genre -> genreService.update(genre),
+                genre -> genreService.delete(genre)
+        );
     }
+
 }
