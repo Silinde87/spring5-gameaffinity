0fed7a2ecb59bc5168620ee59e99be6c99321768
Silinde87 <paurodriguezmolina@gmail.com>
2020-11-15 16:25:11 +0100

    Added bootstap data including description and image url. #Closes 20

--- src/main/java/spring5/silinde87/gameaffinity/backend/bootstrap/DataBootstrap.java
+++ src/main/java/spring5/silinde87/gameaffinity/backend/bootstrap/DataBootstrap.java
@@ -100,6 +100,16 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
         //Creating Mario game
         Game mario = Game.builder().name("Mario").releaseDate(LocalDate.of(2015,10,25))
+                .description("Mario Bros. (マリオブラザーズ, Mario Burazāzu) is a platform game published and developed for " +
+                        "arcades by Nintendo in 1983. It was created by Shigeru Miyamoto. It has been featured as a minigame " +
+                        "in the Super Mario Advance series and numerous other games. Mario Bros. has been re-released for the " +
+                        "Wii's, Nintendo 3DS's, and Wii U's Virtual Console services in Japan, North America, Europe and Australia.\n" +
+                        "In the game, Mario is portrayed as an Italian-American plumber who, along with his brother Luigi, has to defeat " +
+                        "creatures that have been coming from the sewers. The gameplay focuses on Mario and Luigi exterminating the " +
+                        "creatures by flipping them on their backs and kicking them away. The original versions of Mario Bros.—the " +
+                        "arcade version and the Family Computer/Nintendo Entertainment System (FC/NES) version—were received " +
+                        "positively by critics.")
+                .imageUrl("https://media.rawg.io/media/resize/1920/-/screenshots/ea8/ea8078bbe72f544be35c7616787ecb15.jpg")
                 .developer(atlusDeveloper)
                 .producer(eijiProducer)
                 .genre(actionGenre)
@@ -115,6 +125,18 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
         //Creating Gears of War game
         Game gow = Game.builder().name("Gears of War").releaseDate(LocalDate.of(2010,5,20))
+                .description("The game from the famous game designer Cliff Bleszinski. Gears of War for many years has " +
+                        "become the hallmark of the game console from Microsoft. Gears of War is a third-person shooter " +
+                        "that uses a cover system from the game Kill Switch as the main game mechanics: the protagonist " +
+                        "sticks to the walls, ledges and other elevations on the map, shooting back at the enemies. This " +
+                        "is the first game of the series.\n" + "\n" +"The action takes place on the exoplanet Sera. On it, " +
+                        "there is a stronghold of human civilization, which after a thousand-year conflict is on the verge " +
+                        "of collapse. Underneath all the main human cities, dwells the Locust Horde - inhuman monsters " +
+                        "the size of a truck.\n" +"\n" +"The player becomes a soldier Marcus Phoenix from the Coalition " +
+                        "of Ordered Governments. The action begins 14 years after the first contact: the prison in which " +
+                        "the Phoenix was kept attacked by The Horde. An old friend helps Marcus to get out and thus begins " +
+                        "an adventure to save the world from chthonic invaders")
+                .imageUrl("https://media.rawg.io/media/resize/1920/-/screenshots/854/854f930ed3b39cfd00eca91b7a99c7de.jpg")
                 .developer(bizarreDeveloper)
                 .producer(hideoProducer)
                 .genre(adventureGenre)
@@ -132,6 +154,12 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
         //Creating Gears of War game
         Game halo = Game.builder().name("Halo 3").releaseDate(LocalDate.of(1977,6,18))
+                .description("Halo 3 is a traditional first-person shooter, with a well-designed game world and mythology. " +
+                        "The series is known for its multiplayer, which many consider the best in the genre. Matches can " +
+                        "fit up to 16 people, who fight in several game modes. They are a variation on the Deathmatch and " +
+                        "Capture the Flag modes. In addition to multiplayer battles, players can go through the campaign " +
+                        "together with 3 friends in a cooperative mode.")
+                .imageUrl("https://media.rawg.io/media/resize/1920/-/screenshots/109/1099c73ce1ed6023ac8bcafb468aa024.jpg")
                 .developer(blizzardDeveloper)
                 .producer(jonathanProducer)
                 .genre(rpgGenre)
